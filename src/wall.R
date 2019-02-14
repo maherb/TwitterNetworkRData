f448edfa8e7db2ff9c62797b05c49887bcae0f33
Brian Maher <maherb@rpi.edu>
2019-02-14 02:56:49 -0500

    Floor now kind of works

--- src/wall.R
+++ src/wall.R
@@ -43,14 +43,14 @@ UpdateColumn <- function(data_subset, current_node_data, queries) {
                   overflow-x: hidden;',
                            if(nrow(data_subset) > 0) {
                              lapply(1:nrow(data_subset), function(tweet_num) {
-                               #colored.text <- colorHashtags(data_subset$text[[tweet_num]],
-                               #                               queries,
-                               #                              data_subset$hashtags[[tweet_num]],
-                               #                               c(data_subset$urls_t.co[[tweet_num]], data_subset$ext_media_t.co[[tweet_num]]),
-                               #                              data_subset$mentions_screen_name[[tweet_num]])
+                               #colored.text <- colorHashtags(data_subset$full_text[tweet_num],
+                              #                                queries,
+                              #                               data_subset$hashtags[tweet_num],
+                              #                               c(),# c(data_subset$urls_t.co[tweet_num], data_subset$ext_media_t.co[tweet_num]),
+                              #                               c())#data_subset$mentions_screen_name[tweet_num])
                               tags$div(style = 'padding: 0px;',
                                         tags$h3(tags$span(class = "clickable", paste0("@", data_subset$screen_name[tweet_num]))),
-                                        tags$p(HTML(data_subset$full_text[tweet_num])),
+                                        tags$p(HTML(data_subset$full_text[tweet_num])),#colored.text)),
                                         tags$p(HTML(paste("&#x1F499", data_subset$favorite_count[tweet_num], "&#x1F504", data_subset$retweet_count[tweet_num])))
                                )
                              })
@@ -76,20 +76,20 @@ colorHashtags <- function(string, queries, hashtags, urls, mentions) {
   hashtags <- hashtags[order(nchar(hashtags), hashtags, decreasing = TRUE)]
   mentions <- mentions[order(nchar(mentions), mentions, decreasing = TRUE)]
   for(hashtag in hashtags) {
-    if(toupper(paste0("#", hashtag)) %in% toupper(queries)) {
-      replacement <- paste0('<span class="clickable included">', paste0("#&", hashtag), '</span>')
-    } else {
-      replacement <- paste0('<span class="clickable notincluded">', paste0("#&", hashtag), '</span>')
-    }
+    #if(toupper(paste0("#", hashtag)) %in% toupper(queries)) {
+    #  replacement <- paste0('<span class="clickable included">', paste0("#&", hashtag), '</span>')
+    #} else {
+    replacement <- paste0('<span class="clickable notincluded">', paste0("#&", hashtag), '</span>')
+    #}
     string <- str_replace_all(string, paste0("#", hashtag), replacement)
   }
   string <- str_replace_all(string, "#&", "#")
   for(mention in mentions) {
-    if(toupper(paste0("@", mention)) %in% toupper(queries)) {
-      replacement <- paste0('<span class="clickable mentionincluded">', paste0("@&", mention), '</span>')
-    } else {
-      replacement <- paste0('<span class="clickable notincluded">', paste0("@&", mention), '</span>')
-    }
+    #if(toupper(paste0("@", mention)) %in% toupper(queries)) {
+    # replacement <- paste0('<span class="clickable mentionincluded">', paste0("@&", mention), '</span>')
+    #} else {
+    replacement <- paste0('<span class="clickable notincluded">', paste0("@&", mention), '</span>')
+    #}
     string <- str_replace_all(string, paste0("@", mention), replacement)
   }
   string <- str_replace_all(string, "@&", "@")
