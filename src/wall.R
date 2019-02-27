636d8597f69b661994920d68f5b6dd11d51d0396
Brian Maher <maherb@rpi.edu>
2019-02-26 19:02:27 -0500

    More elegant mention highlighting that is case insensitive

--- src/wall.R
+++ src/wall.R
@@ -63,18 +63,12 @@ UpdateColumn <- function(data_subset, current_node_data, queries) {
   )
 }
 
-# case insensitive, though it can only highlight one varation of each handle, i.e.
-# if there is "@BrIaN" and "@bRIAN", it will only light whichever one happens first. 
 highlightMentions <- function(string, mentions) {
   if (!is.null(mentions)) {
     mentions <- mentions[order(nchar(mentions), mentions, decreasing = TRUE)]
     for(mention in mentions) {
       replacement <- paste0('<span class="clickable mentionincluded">', "@&", mention, '</span>')
-      mentionPosInfo <- regexpr(paste0("@", tolower(mention)), tolower(string), fixed = TRUE)
-      mentionPos <- mentionPosInfo[1]
-      mentionText <- substr(string, mentionPos, mentionPos + nchar(mention))
-      print(mentionText)
-      string <- str_replace_all(string, mentionText, replacement)
+      string <- gsub(paste0('@', mention), replacement, string, ignore.case = TRUE)
     }
     string <- str_replace_all(string, "@&", "@")
   }
